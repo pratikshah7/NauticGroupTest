8690a3cf3008d64fd6c80b9f0eb718d34c4c9866
pratikshah7 <88850740+pratikshah7@users.noreply.github.com>
2021-08-12 16:51:32 -0400

    Update SensorData.c

--- SensorData.c
+++ SensorData.c
@@ -12,7 +12,7 @@ int main()
     // It is assumed that Sensor data is in the SensorData.txt file
     // Data is stored in Optimized.txt after optimization [reduced bytes]
     OptimizeSensorData();
-
+    //Data will be retrieved from optimized.txt and original data pattern is generated and stored in retriveddata.txt
     RetriveSensorData();
 
     return 0;
